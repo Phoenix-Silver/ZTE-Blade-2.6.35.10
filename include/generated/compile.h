d23d2013e7d7b47d806bca0fe7e8a06f363145b8
Tom Brennan <tommie.brennan@gmail.com>
2011-07-04 17:59:07 +0100

    Modified .config to get the compile to work

--- include/generated/compile.h
+++
@@ -1,9 +0,0 @@
-/* This file is auto generated, version 6 */
-/*  PREEMPT */
-#define UTS_MACHINE "arm"
-#define UTS_VERSION "#6 PREEMPT Sun Jul 3 13:45:01 CEST 2011"
-#define LINUX_COMPILE_TIME "13:45:01"
-#define LINUX_COMPILE_BY "lg-electronics"
-#define LINUX_COMPILE_HOST "phoenix"
-#define LINUX_COMPILE_DOMAIN "(none)"
-#define LINUX_COMPILER "gcc version 4.4.0 (GCC) "
